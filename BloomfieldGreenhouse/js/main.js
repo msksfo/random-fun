bc5bb965b5d26f52463c04eeac509fd50252d4b9
Tia Esguerra <msksfo@gmail.com>
2018-11-10 11:12:19 -0800

    Insert copyright year via javascript so it will always be current

--- BloomfieldGreenhouse/js/main.js
+++ BloomfieldGreenhouse/js/main.js
@@ -1,11 +1,20 @@
+
+
+// So the copyright date will always be the current year
+const year = document.querySelector('.year');
+let date = new Date();
+year.innerHTML = date.getFullYear();
+
+
 const logoLink = document.getElementById('logo-link');
+logoLink.addEventListener('click', scrollToTop);
 
-logoLink.addEventListener('click', function(){
-    window.scroll(0, 0);
-});
-
-logoLink.addEventListener('keypress', function(e){
-    if (e.keyCode === 13){
+logoLink.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
         window.scroll(0, 0);
     }
 });
+
+function scrollToTop() {
+    window.scroll(0, 0)
+}
