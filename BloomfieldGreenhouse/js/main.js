638ddcaff875e80b2bb3fb20599ae95532c76602
Tia Esguerra <msksfo@gmail.com>
2018-12-22 12:45:50 -0800

    feat: Add all code for opening and closing the modal window

--- BloomfieldGreenhouse/js/main.js
+++ BloomfieldGreenhouse/js/main.js
@@ -6,6 +6,7 @@ const plantsContent = document.querySelector('.plants-content');
 const plantsModal = document.querySelector('.plants-modal');
 const modalItems = document.querySelectorAll('.modal-item');
 const closeModalButton = document.querySelector('.close-modal-button');
+let lastFocusedElement;
 
 // So the copyright date will always be the current year
 const year = document.querySelector('.year');
@@ -20,12 +21,57 @@ function scrollToTop() {
     window.scroll(0, 0)
 }
 
+function showModal(e) {
+    plantsModal.classList.add('plants-modal-expanded');
+
+    // TODO: only show the modal items of the parent that was clicked
+    modalItems.forEach(value => value.classList.add('show-modal-content'));
+
+    // make the modal visible to screen readers
+    plantsModal.setAttribute('aria-hidden', false);
+
+    if (e.type === 'keydown') {
+        // remember which element was focused before opening the modal
+        lastFocusedElement = document.activeElement;
+
+        // set focus on the close button when the modal is opened
+        closeModalButton.focus();
+
+        // keyboard trap to prevent tabbing outside the modal
+        plantsModal.addEventListener('keydown', function (e) {
+            if (e.keyCode === 9) {
+                e.preventDefault()
+            }
+        })
+    }
+
+}
+
+function hideModal(e) {
+
+    // QUICKLY transition to hide the modal content when the modal is closed
+    modalItems.forEach(value => value.classList.add('hide-modal-content'));
+
+    // close the modal, remove the class that shows the modal content
+    modalItems.forEach(value => value.classList.remove('show-modal-content'));
+
+    plantsModal.classList.remove('plants-modal-expanded');
+
+    // hide the modal from screen readers
+    plantsModal.setAttribute('aria-hidden', true);
+
+    if (e.type === 'keydown') {
+        // return focus to the element that held focus before the modal was open 
+        lastFocusedElement.focus();
+    }
+
+}
 
 /******** EVENT LISTENERS ********/
 
 logoLink.addEventListener('click', scrollToTop);
 
-logoLink.addEventListener('keypress', function (e) {
+logoLink.addEventListener('keydown', function (e) {
     if (e.keyCode === 13) {
         window.scroll(0, 0);
     }
@@ -33,32 +79,55 @@ logoLink.addEventListener('keypress', function (e) {
 
 
 plantsContent.addEventListener('click', function (e) {
-    console.log(e.target.classList.value)
-    console.log(plantsModal);
 
-    // make sure the animation is only triggered if an h2 was clicked
+    // make sure the animation is only triggered if the open modal button was clicked
     if (e.target.classList.value === 'open-modal-alternate') { // .plant-title or .open-modal???
-        plantsModal.classList.add('plants-modal-expanded');
 
-        // TODO: only show the modal items of the parent that was clicked
-        modalItems.forEach(value => value.classList.add('show-modal-content'));
+        showModal(e);
+    } else if (e.target.classList.value === 'close-modal-button') {
+
+        hideModal(e)
     }
 });
 
 
-closeModalButton.addEventListener('click', function (e) {
-    // QUICKLY transition to hide the modal content when the modal is closed
-    modalItems.forEach(value => value.classList.add('hide-modal-content'));
+plantsContent.addEventListener('keydown', function (e) {
 
-    // close the modal, remove the class that shows the modal content
-    modalItems.forEach(value => value.classList.remove('show-modal-content'));
-    plantsModal.classList.remove('plants-modal-expanded');
+    // allow the user to open the modal by pressing 'enter', or the space bar
+    if ((e.target.classList.value === 'open-modal-alternate') && (e.keyCode === 13 || e.keyCode === 32)) {
+        // prevent default behavior of enter and space
+        e.preventDefault();
+
+        showModal(e);
+
+    } else {
+        if ((e.target.classList.value === 'close-modal-button') && (e.keyCode === 32)) {
+            // prevent the default behavior of the space key
+            e.preventDefault();
+
+            hideModal(e);
+
+        } else if ((e.target.classList.value === 'close-modal-button') && (e.keyCode === 13 || e.keyCode === 27)) {
+
+            // prevent the default behavior of enter
+            e.preventDefault();
+
+            hideModal(e);
+        }
+    }
 });
 
 
-// when the modal transition has ended, remove the class that adds the QUICK transition to hide modal content
-plantsModal.addEventListener('transitionend', function (e) {
-    modalItems.forEach(value => value.classList.remove('hide-modal-content'));
+
+/*
+    when the transition to close the modal window (by removing the class .plants-modal-expanded) is complete, remove the class that adds the QUICK transition to hide modal content
+*/
+plantsContent.addEventListener('transitionend', function (e) {
+    // conditionals to ensure the event only fires on the intended transition
+    if (e.propertyName === 'transform' && e.target.classList.value === 'plants-modal') {
+        modalItems.forEach(value => value.classList.remove('hide-modal-content'));
+    }
+
 })
 
 
