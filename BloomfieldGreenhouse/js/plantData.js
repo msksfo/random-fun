edc6ea618e931ec5ca0d79fd37a9af447138f012
Tia Esguerra <msksfo@gmail.com>
2018-12-23 18:34:17 -0800

    refactor: Move the plant data for the modal window into it's own file

---
+++ BloomfieldGreenhouse/js/plantData.js
@@ -0,0 +1,36 @@
+const plantInfo = (function () {
+
+
+    const info = {
+        "Flowers": [
+            {
+                "Sun": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            },
+            {
+                "Sun / Shade": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            },
+            {
+                "Shade": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            }
+        ]
+    }
+
+    return info;
+})()
+
