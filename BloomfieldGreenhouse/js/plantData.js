2e9d993b74abf895c39a6e84f1cb8af550fb2023
Tia Esguerra <msksfo@gmail.com>
2018-12-26 09:22:51 -0800

    wip: Add all plant data as placeholder text

--- BloomfieldGreenhouse/js/plantData.js
+++ BloomfieldGreenhouse/js/plantData.js
@@ -28,6 +28,121 @@ const plantInfo = (function () {
                     "five"
                 ]
             }
+        ],
+        "Perennials &amp; Shrubs": [ // & written &amp; to match html entity code
+            {
+                "Perennials": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            },
+            {
+                "Shrubs": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            }
+        ],
+        "Vegetables": [
+            {
+                "Tomato": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            },
+            {
+                "Pepper": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            },
+            {
+                "Something Else": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            }
+        ],
+        "Herbs": [
+            {
+                "Herb 1": [
+                    "one",
+                    "two",
+                    "three",
+                    "four"
+                ]
+            },
+            {
+                "Herb 2": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            },
+            {
+                "Herb 3": [
+                    "one",
+                    "two",
+                    "three",
+                    "four"
+                ]
+            }
+        ],
+        "Bulk Seeds": [
+            {
+                "Seeds": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            },
+            {
+                "More Seeds": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            }
+        ],
+        "Hanging Baskets": [
+            {
+                "Type 1": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            },
+            {
+                "Type 2": [
+                    "one",
+                    "two",
+                    "three"
+                ]
+            },
+            {
+                "Type 3": [
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five"
+                ]
+            }
         ]
     }
 
