5dcad711108bb96bf70cea512eaa5b03b54bdf6d
Tia Esguerra <msksfo@gmail.com>
2019-01-07 17:52:56 -0800

    feat: Add function to check if video is ready. If not, use photo fallback, and recheck in half a second.

--- BloomfieldGreenhouse/js/landing.js
+++ BloomfieldGreenhouse/js/landing.js
@@ -3,13 +3,20 @@ let videoContainer = document.getElementById('js-video-container');
 
 let mq = window.matchMedia("(min-width: 768px)");
 
-if (mq.matches) {
-    video.addEventListener('loadeddata', function () {
 
+// check if the video is ready. if so, play it. if not, load the photo and check again in half a second
+function checkforVideo() {
+    var videoCheck = setInterval(() => {
         if (video.readyState >= 2) {
             video.play();
+            clearInterval(videoCheck);
         } else {
             videoContainer.classList.remove('video-container-video')
         }
-    });
+    }, 500);
+}
+
+
+if (mq.matches) {
+    video.addEventListener('loadeddata', checkforVideo);
 }
