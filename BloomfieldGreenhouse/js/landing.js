4c0b601782fdc221b947328d7ab527cb69a08a82
Tia Esguerra <msksfo@gmail.com>
2019-01-07 18:04:04 -0800

    refactor: change let to const

--- BloomfieldGreenhouse/js/landing.js
+++ BloomfieldGreenhouse/js/landing.js
@@ -1,22 +1,25 @@
-let video = document.getElementById('js-daisyVideo');
-let videoContainer = document.getElementById('js-video-container');
+(function () {
 
-let mq = window.matchMedia("(min-width: 768px)");
+    const mq = window.matchMedia("(min-width: 768px)");
+    const video = document.getElementById('js-daisyVideo');
+
+    // check if the video is ready. if so, play it. if not, load the photo and check again in half a second
+    function checkforVideo() {
+
+        const videoContainer = document.getElementById('js-video-container');
+
+        const videoCheck = setInterval(() => {
+            if (video.readyState >= 2) {
+                video.play();
+                clearInterval(videoCheck);
+            } else {
+                videoContainer.classList.remove('video-container-video')
+            }
+        }, 500);
+    }
 
 
-// check if the video is ready. if so, play it. if not, load the photo and check again in half a second
-function checkforVideo() {
-    var videoCheck = setInterval(() => {
-        if (video.readyState >= 2) {
-            video.play();
-            clearInterval(videoCheck);
-        } else {
-            videoContainer.classList.remove('video-container-video')
-        }
-    }, 500);
-}
-
-
-if (mq.matches) {
-    video.addEventListener('loadeddata', checkforVideo);
-}
+    if (mq.matches) {
+        video.addEventListener('loadeddata', checkforVideo);
+    }
+})();
