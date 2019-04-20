7f41efce78af537ccf4fa87ee127dbc7048a6317
Tia Esguerra <msksfo@gmail.com>
2019-04-20 09:55:32 -0700

    first commit

---
+++ watch/js/main.js
@@ -0,0 +1,24 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function updateTime() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+
+    const hours = now.getHours();
+    const hoursDegrees = 30 * (hours % 12) + minutes / 60 + 90;
+
+    secondHand.style.transform = 'rotate(' + secondsDegrees + 'deg)';
+
+    minuteHand.style.transform = 'rotate(' + minutesDegrees + 'deg)';
+
+    hourHand.style.transform = 'rotate(' + hoursDegrees + 'deg)';
+}
+
+setInterval(updateTime, 1000);
---
+++ watch/readme.md
@@ -0,0 +1,11 @@
+# Grey Side Of The Moon
+
+## Project Description
+
+This CSS and JavaScript project is a watch inspired by the 'Grey Side Of The Moon' Omega Speedmaster.
+
+![comparison of my project to the real Omega watch]('/images/comparison.jpg')
+
+## Live Link
+
+See the result here <https://msksfo.github.io/random-fun/watch/index.html>
